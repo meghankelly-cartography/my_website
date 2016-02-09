a413c6ba2a7f87e0a7272ed2e3055d68cd2deb97
meghankelly-cartography <mkelly22@wisc.edu>
2016-02-08 21:28:43 -0600

    Module 3 debug problems

--- js/main.js
+++ js/main.js
@@ -1,18 +1,62 @@
+//initialize function called when the script loads
+function initialize(){
+	jQueryAjax();
+	//debugCallback();
+	//debugAjax();
+}
+
+    //define a variable to hold the data
+    //define data variable globally to be used in all functions
+    var mydata;
+
 //define AJAX function
 function jQueryAjax(){
+
     //basic jQuery ajax method
-    $.ajax("data/MegaCities.geojson", {
+    $.ajax("data/Cities.geojson", {
         dataType: "json",
-        success: callback
+        success: function(response){
+            mydata = response;
+
+            //check the data
+            //executed within the callback, after data is loaded and assigned
+            console.log(mydata);
+        }
     });
+
+    //check the data
+    //executed first, before the data is loaded and returns undefined
+    console.log(mydata);
+    
+
+
+//debug portion of assignment
+function debugCallback(response){
+	
+	$(mydiv).append('GeoJSON data: ' + JSON.stringify(mydata));
 };
 
-//define callback function
-function callback(response, status, jqXHRobject){
+function debugAjax(){
+	
+	$.ajax("data/Cities.geojson", {
+		dataType: "json",
+		success: function(response){
+				console.log("This runs");
 
-    //TASKS USING THE DATA GO HERE
-    console.log(response);
+			debugCallback(mydata);
+		}
+	});
+
+	$(mydiv).append('<br>GeoJSON data:<br>' + JSON.stringify(mydata));
+};
+
+$(mydiv).append('GeoJSON data: ' + JSON.stringify(mydata));
+
+
 
 };
 
-$(document).ready(jQueryAjax);
+
+
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
